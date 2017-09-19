9966aa6c18b3beadaa6b02df651c0ba93bae3d4f
maria.burtseva <maria.burtseva@billing.ru>
2017-09-19 13:36:24 +0300

    переименованы Distance и Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
@@ -1,20 +1,19 @@
 package ru.stqa.pft.sandbox;
 
 public class Distance {
+  public static void main(String[] args) {
 
-    public double x;
-    public double y;
+    Point x = new Point(3, 5);
+    Point y = new Point(6, 9);
 
-  public Distance(double x, double y)
-  {
-    this.x=x;
-    this.y=y;
+    //вызов функции
+    System.out.println("Расстояние между точками (функция) "+ "x "+ x.x + ";"+ x.y + " и "+"y "+ y.x + ";"+ y.y + " = " + distance(x,y));
+
+    //вызов метода
+    System.out.println("Расстояние между точками (метод) "+ "x "+ x.x + ";"+ x.y + " и "+"y "+ y.x + ";"+ y.y + " = " + y.getDistance(x));
   }
 
-  public double getdistance(Distance p) {
-    return Math.sqrt((x - p.x)*(x - p.x)+(y - p.y)*(y - p.y));
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt((p2.x - p1.x)*(p2.x - p1.x)+(p2.y -p1.y)*(p2.y - p1.y));
   }
-
-  }
-
-
+}
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,19 +1,20 @@
 package ru.stqa.pft.sandbox;
 
 public class Point {
-  public static void main(String[] args) {
 
-    Distance x = new Distance(3, 5);
-    Distance y = new Distance(6, 9);
+    public double x;
+    public double y;
 
-    //вызов функции
-    System.out.println("Расстояние между точками (функция) "+ "x "+ x.x + ";"+ x.y + " и "+"y "+ y.x + ";"+ y.y + " = " + distance(x,y));
-
-    //вызов метода
-    System.out.println("Расстояние между точками (метод) "+ "x "+ x.x + ";"+ x.y + " и "+"y "+ y.x + ";"+ y.y + " = " + y.getdistance(x));
+  public Point(double x, double y)
+  {
+    this.x=x;
+    this.y=y;
   }
 
-  public static double distance(Distance p1, Distance p2) {
-    return Math.sqrt((p2.x - p1.x)*(p2.x - p1.x)+(p2.y -p1.y)*(p2.y - p1.y));
+  public double getDistance(Point p) {
+    return Math.sqrt((x - p.x)*(x - p.x)+(y - p.y)*(y - p.y));
   }
-}
+
+  }
+
+
