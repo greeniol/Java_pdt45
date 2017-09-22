c8516845eddfa25dec98a12a7940adafd87257e2
maria.burtseva <maria.burtseva@billing.ru>
2017-09-22 09:52:56 +0300

    Объединенные и отрефакторенные тесты для создания группы и контакта.

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,37 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+  private final String name;
+  private final String lastname;
+  private final String address;
+  private final String mail;
+  private final String phone;
+
+  public ContactData(String name, String lastname, String address, String mail, String phone) {
+    this.name = name;
+    this.lastname = lastname;
+    this.address = address;
+    this.mail = mail;
+    this.phone = phone;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getMail() {
+    return mail;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+}
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupAndContactTests.java
@@ -0,0 +1,120 @@
+package ru.stqa.pft.addressbook;
+
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+
+public class GroupAndContactTests {
+    FirefoxDriver wd;
+    
+    @BeforeMethod
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        login("admin", "secret");
+    }
+
+    private void login(String user, String password) {
+        wd.get("http://localhost/addressbook/group.php");
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys(user);
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys(password);
+        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
+
+    @Test
+    public void TestCreationGroupAndContact() {
+
+        gotoGroupPage();
+        initGroupCreation();
+        fillGroupForm(new GroupData("Test1", "Test2", "Test3"));
+        submitGroupCreation();
+        returnToGroupPage();
+        addNewContact();
+        fillContactForm(new ContactData("First", "Contact", "Street home 88", "mail@mail.con", "74445551122"));
+        submitNewContact();
+        returnToHome();
+    }
+
+    private void returnToHome() {
+        wd.findElement(By.linkText("home")).click();
+    }
+
+    private void submitNewContact() {
+        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    }
+
+    private void fillContactForm(ContactData contactData) {
+        wd.findElement(By.name("firstname")).click();
+        wd.findElement(By.name("firstname")).clear();
+        wd.findElement(By.name("firstname")).sendKeys(contactData.getName());
+        wd.findElement(By.name("lastname")).click();
+        wd.findElement(By.name("lastname")).clear();
+        wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
+        wd.findElement(By.name("address")).click();
+        wd.findElement(By.name("address")).clear();
+        wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
+        wd.findElement(By.name("email")).click();
+        wd.findElement(By.name("email")).clear();
+        wd.findElement(By.name("email")).sendKeys(contactData.getMail());
+        wd.findElement(By.name("theform")).click();
+        wd.findElement(By.name("home")).click();
+        wd.findElement(By.name("home")).clear();
+        wd.findElement(By.name("home")).sendKeys(contactData.getPhone());
+    }
+
+    private void addNewContact() {
+        wd.findElement(By.linkText("add new")).click();
+    }
+
+    private void returnToGroupPage() {
+        wd.findElement(By.linkText("group page")).click();
+    }
+
+    private void submitGroupCreation() {
+        wd.findElement(By.name("submit")).click();
+    }
+
+    private void fillGroupForm(GroupData groupData) {
+        wd.findElement(By.name("group_name")).click();
+        wd.findElement(By.name("group_name")).clear();
+        wd.findElement(By.name("group_name")).sendKeys(groupData.getGroupname());
+        wd.findElement(By.name("group_header")).click();
+        wd.findElement(By.name("group_header")).clear();
+        wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+        wd.findElement(By.name("group_footer")).click();
+        wd.findElement(By.name("group_footer")).clear();
+        wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+    }
+
+    private void initGroupCreation() {
+        wd.findElement(By.name("new")).click();
+    }
+
+    private void gotoGroupPage() {
+        wd.findElement(By.linkText("groups")).click();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        wd.quit();
+    }
+    
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
