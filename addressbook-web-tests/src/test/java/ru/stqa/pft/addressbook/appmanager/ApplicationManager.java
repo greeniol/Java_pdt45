219f3a32d0e9399dba1871b81892bdf649a7493f
maria.burtseva <maria.burtseva@billing.ru>
2017-09-28 17:08:07 +0300

    Возможность работы с 3 браузерами.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -2,23 +2,39 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import ru.stqa.pft.addressbook.model.ContactData;
+import sun.plugin2.util.BrowserType;
 
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-  FirefoxDriver wd;
+  WebDriver wd;
   private ContactHelper contactHelper;
   private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
+  private String browser;
 
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
 
   public void init() {
-    wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+    if(Objects.equals(browser, org.openqa.selenium.remote.BrowserType.FIREFOX)){
+      wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+    }else if(Objects.equals(browser, org.openqa.selenium.remote.BrowserType.CHROME)){
+      wd= new ChromeDriver();
+    }else if(Objects.equals(browser, org.openqa.selenium.remote.BrowserType.IE)){
+      wd= new InternetExplorerDriver();
+    }
+
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/group.php");
     groupHelper = new GroupHelper(wd);
