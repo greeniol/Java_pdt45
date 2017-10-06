1cbfa9085bbccee2427c142315219f5bef7a312a
maria.burtseva <maria.burtseva@billing.ru>
2017-10-06 13:50:26 +0300

    Удаление/редактирование последнего элемента.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -13,7 +13,7 @@ public class GroupModificationTests extends TestBase{
     if(! app.getGroupHelper().isThereAGroup()){
       app.getGroupHelper().createGroup(new GroupData("Test6", null, null));
     }
-    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().selectGroup(before-1);
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("Test99", "Test11", "Test33"));
     app.getGroupHelper().submitGroupModification();
