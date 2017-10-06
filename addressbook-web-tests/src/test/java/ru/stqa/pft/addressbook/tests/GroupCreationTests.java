3ae81e0b0802ce1b8bdef1066d99ccb826880196
maria.burtseva <maria.burtseva@billing.ru>
2017-10-06 11:57:56 +0300

    Добавление подсчета элементов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,17 +1,20 @@
 package ru.stqa.pft.addressbook.tests;
 
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 public class GroupCreationTests extends TestBase {
 
-    @Test
-    public void TestCreationGroup() {
+  @Test
+  public void TestCreationGroup() {
+    app.getNavigationHelper().gotoGroupPage();
+    int before = app.getGroupHelper().getGroupCount();
+    app.getGroupHelper().createGroup(new GroupData("Test6", null, null));
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before+1);
 
-        app.getNavigationHelper().gotoGroupPage();
-        app.getGroupHelper().createGroup(new GroupData("Test6", null, null));
-
-    }
+  }
 
 }
