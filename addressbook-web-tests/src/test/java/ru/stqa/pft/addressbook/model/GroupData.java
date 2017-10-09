90e5e9e72fe79616a35eada8964f3abe06df9f16
maria.burtseva <maria.burtseva@billing.ru>
2017-10-09 09:47:35 +0300

    Сравнение коллекций удаление/редактирование

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,11 +1,21 @@
 package ru.stqa.pft.addressbook.model;
 
-public class GroupData {
+public class
+GroupData {
+  private final String id;
   private final String groupname;
   private final String header;
   private final String footer;
 
-  public GroupData(String groupname, String header, String footer) {
+  public GroupData( String groupname, String header, String footer) {
+    this.id = null;
+    this.groupname = groupname;
+    this.header = header;
+    this.footer = footer;
+  }
+
+  public GroupData(String id, String groupname, String header, String footer) {
+    this.id = id;
     this.groupname = groupname;
     this.header = header;
     this.footer = footer;
@@ -22,4 +32,36 @@ public class GroupData {
   public String getFooter() {
     return footer;
   }
+
+  public String getId() {
+    return id;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    GroupData groupData = (GroupData) o;
+
+    if (id != null ? !id.equals(groupData.id) : groupData.id != null) return false;
+    return groupname != null ? groupname.equals(groupData.groupname) : groupData.groupname == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = id != null ? id.hashCode() : 0;
+    result = 31 * result + (groupname != null ? groupname.hashCode() : 0);
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return "GroupData{" +
+            "id='" + id + '\'' +
+            ", groupname='" + groupname + '\'' +
+            '}';
+  }
+
+
 }
