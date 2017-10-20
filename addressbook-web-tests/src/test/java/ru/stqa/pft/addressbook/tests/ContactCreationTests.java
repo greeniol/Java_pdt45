802c4d2184acd914122ef37e7334a857fb5ea08b
maria.burtseva <maria.burtseva@billing.ru>
2017-10-20 16:38:19 +0300

    параметризация данных для контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,11 +1,22 @@
 package ru.stqa.pft.addressbook.tests;
 
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
+import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.*;
@@ -13,19 +24,24 @@ import static org.hamcrest.MatcherAssert.*;
 
 public class ContactCreationTests extends TestBase {
 
-  @Test
-  public void TestCreationContact() {
+  @DataProvider
+  public Iterator<Object[]> validContactsFromJson() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+    String json ="";
+    String line = reader.readLine();
+    while (line != null){
+      json+=line;
+      line = reader.readLine();
+    }
+    Gson gson= new Gson();
+    List<ContactData> contacts =gson.fromJson(json,new TypeToken<List<ContactData>>(){}.getType());
+    XStream xstream = new XStream();
+    return contacts.stream().map((c)->new Object[] {c}).collect(Collectors.toList()).iterator();
+  }
+
+  @Test(dataProvider = "validContactsFromJson")
+  public void TestCreationContact(ContactData contact) {
     Contacts before = app.contact().all();
-    File photo = new File("src/test/resources/2.png");
-    ContactData contact = new ContactData().withName("First").withLastname("Contact")
-            .withAddress("Street " + "              " +
-                    "home                      " +
-                    "88           " +
-                    "fajwerawraiwerqottttttttttttttttttt")
-            .withMail("mail@mail.con").withMail2("ree_rfsf@fjfjf.re").withMail3("EW2ff@gdg.com")
-            .withHomephone("+7(444)55511").withMobilephone("787-3333").withWorkphone("74 66 61")
-            .withGroup("Test2")
-            .withPhoto(photo);
     app.contact().create(contact, true);
     app.goTo().returnToHome();
     Contacts after = app.contact().all();
