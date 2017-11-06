62ea4e290b325741d26735c852faa8c3c906de34
maria.burtseva <maria.burtseva@billing.ru>
2017-11-06 15:22:22 +0300

    Добавление в группу

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactInGroup.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactInGroup.java
@@ -7,53 +7,69 @@ import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
-import java.util.stream.Collectors;
+import java.util.Iterator;
 
-import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.testng.Assert.assertEquals;
 
 
-public class ContactInGroup extends TestBase{
+public class ContactInGroup extends TestBase {
   @BeforeMethod
   public void ensurePreconditionsContacts() {
-    if (app.db().contacts().size() == 0) {
+    if (app.db().contacts().size() == 0 || contactsWithFullGroups() == app.db().contacts().size()) {
       app.goTo().returnToHome();
       app.contact().create(new ContactData().withName("First").withLastname("Contact").withAddress("Street home 88")
               .withMail("mail@mail.con").withHomephone("+7(444)55511").withMobilephone("787-3333").withWorkphone("74 66 61"), false);
       app.goTo().returnToHome();
     }
   }
+
   @BeforeMethod
   public void ensurePreconditionsGroups() {
     if (app.db().groups().size() == 0) {
       app.goTo().GroupPage();
-      app.group().create(new GroupData().withGroupname("Test2"));
+      app.group().create(new GroupData().withGroupname("Test 2"));
+      app.goTo().returnToHome();
     }
   }
 
+
+  public static int contactsWithFullGroups() {
+    Groups groups = app.db().groups();
+    Contacts before = app.db().contacts();
+    int contactsWithFullGroup = 0;
+    Iterator<ContactData> contacts = before.iterator();
+    while (contacts.hasNext()) {
+      ContactData contact = contacts.next();
+      if (contact.getGroups().size() == groups.size()) {
+        contactsWithFullGroup = ++contactsWithFullGroup;
+      }
+    }
+    return contactsWithFullGroup;
+  }
+
+
   @Test
   public void testContactInGroup() {
-    Groups groups = app.db().groups();
-    GroupData group = groups.iterator().next();
+    Groups allgroups = app.db().groups();
     Contacts before = app.db().contacts();
-    ContactData contact = before.iterator().next();
-    if (contact.getGroups().size() != groups.size()) {
-    //  before.remove(contact);
-      //contact = before.iterator().next();
-
-    if (contact.getGroups().contains(group)) {
-      System.out.println("содержит");
-      groups.remove(group);
-      group = groups.iterator().next();
-    }
-
-
-      app.contact().pushInGroup(contact, group);
-      app.goTo().returnToHome();
+    Iterator<ContactData> contacts = before.iterator();
+    Iterator<GroupData> groups = allgroups.iterator();
+    while (contacts.hasNext()) {
+      ContactData contact = contacts.next();
+      if (contact.getGroups().size() != allgroups.size()) {
+        while (groups.hasNext()) {
+          GroupData group = groups.next();
+          if (!contact.getGroups().contains(group))
+          {app.contact().pushInGroup(contact, group);
+            app.goTo().returnToHome();
+            break;
+          }
+        }
+        break;
+      }
     }
     Contacts after = app.db().contacts();
-    System.out.println(after);
-
+    assertEquals(before.size(), after.size());
   }
 }
